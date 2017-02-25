798443855de03df16b12575650c99888aa6d5db6
Emil Suleymanov <suleymanovemil8@gmail.com>
2017-02-25 14:05:02 +0400

    init

---
+++ app/build/obj/local/arm64-v8a/objs/WritePadReco/interface.o.d
@@ -0,0 +1,26 @@
+build/obj/local/arm64-v8a/objs/WritePadReco/interface.o: \
+ src/main/jni/interface.c \
+ src/main/jni/../../../../../include/InkWrapper.h \
+ src/main/jni/../../../../../include/RecognizerApi.h \
+ src/main/jni/../../../../../include/gestures.h \
+ src/main/jni/../../../../../include/recotypes.h \
+ src/main/jni/../../../../../include/recodefs.h \
+ src/main/jni/../../../../../include/langid.h \
+ src/main/jni/../../../../../include/RecognizerWrapper.h \
+ src/main/jni/../../../../../include/InkWrapper.h
+
+src/main/jni/../../../../../include/InkWrapper.h:
+
+src/main/jni/../../../../../include/RecognizerApi.h:
+
+src/main/jni/../../../../../include/gestures.h:
+
+src/main/jni/../../../../../include/recotypes.h:
+
+src/main/jni/../../../../../include/recodefs.h:
+
+src/main/jni/../../../../../include/langid.h:
+
+src/main/jni/../../../../../include/RecognizerWrapper.h:
+
+src/main/jni/../../../../../include/InkWrapper.h:
---
+++ app/src/main/java/com/phatware/android/CustomAdapter.java
@@ -0,0 +1,101 @@
+package com.phatware.android;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.phatware.android.recotest.Wand;
+import com.phatware.android.recotest.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by muradm373 on 8/26/2016.
+ */
+public class CustomAdapter extends BaseAdapter{
+    Context context;
+    ArrayList<Integer> imageId;
+    private static LayoutInflater inflater=null;
+    Wand mainAct;
+
+
+
+    public CustomAdapter(Wand mainActivity, ArrayList<Integer> prgmImages) {
+        // TODO Auto-generated constructor stub
+
+        context=mainActivity;
+        imageId=prgmImages;
+        mainAct = mainActivity;
+        inflater = ( LayoutInflater )context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return imageId.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    public class Holder
+    {
+        ImageView img;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        Holder holder=new Holder();
+        final View rowView;
+
+        rowView = inflater.inflate(R.layout.componentcolor, null);
+
+
+        holder.img=(ImageView) rowView.findViewById(R.id.image);
+
+        ImageView round = (ImageView) rowView.findViewById(R.id.image);
+
+        Drawable shape = round.getBackground();
+        GradientDrawable colorShape = (GradientDrawable)shape;
+        colorShape.setColor(imageId.get(position));
+        holder.img.setBackground(colorShape);
+
+
+
+
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                mainAct.setBrushColor(imageId.get(position));
+            }
+        });
+
+        return rowView;
+    }
+}
